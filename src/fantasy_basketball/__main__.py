7cbeeb87df99126c44d01997d7699705c608b7b6
Tyler Urban <tyurban@gmail.com>
2023-12-17 16:12:04 -0500

    build: Setup main with script to run
    run with `hatch run fantasy_basketball`

---
+++ src/fantasy_basketball/__main__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023-present Tyler Urban <tyurban@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from asyncio import run
+
+
+async def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    run(main())
